7594bf88e21d5ea7dfc9d8756e4391751dfb51eb
Tiago Stutz <tiago.stutz@ingka.ikea.com>
2023-03-26 12:00:26 +0200

    test: smal adjustments

--- serverless-workflow/src/test/java/stutz/tiago/GreetTest.java
+++ serverless-workflow/src/test/java/stutz/tiago/GreetTest.java
@@ -8,6 +8,7 @@ import io.restassured.http.ContentType;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 
 @QuarkusTest
 public class GreetTest {
@@ -17,14 +18,16 @@ public class GreetTest {
         given()
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
-                .body("{\"workflowdata\" : {\"name\" : \"Yoda\", \"country\":\"United States\", \"reference_currency\":\"GBP\"}}")
+                .body("{\"workflowdata\" : {\"name\" : \"Yoda\", \"country\":\"United States\", \"reference_currency\":\"BRL\"}}")
                 .when()
                 .post("/greeting")
                 .then()
                 .statusCode(201)
                 .body("workflowdata.greeting", containsString("Greetings"), "workflowdata.city",
                         containsString("New York"), "workflowdata.state_code", containsString("NY"),
-                        "workflowdata.food_prices.size()", is(1));
+                        "workflowdata.food_prices.size()", is(1),
+                        "workflowdata.food_prices[0].priceInReferenceCurrency",
+                        notNullValue());
     }
 
     @Test
